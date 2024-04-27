dca0b476666eecc12831121fe006fafcb32727b3
Kruti Rathod <swapsmagic83@gmail.com>
2024-04-26 23:57:22 -0700

    light-out exercise adding

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Board from "./Board";
+import "./App.css";
+
+/** Simple app that just shows the LightsOut game. */
+
+function App() {
+  return (
+      <div className="App">
+        <h1>Home</h1>
+        <Board />
+      </div>
+  );
+}
+
+export default App;
